6a8e1aba5775377e0c64cffd74270f9d309c0fbf
fuutot <fuutt659@gmail.com>
2024-08-06 19:25:32 +0900

    add easy_func

---
+++ easy_func/increment.js
@@ -0,0 +1,5 @@
+function increment(num) {
+    return num + 1;
+}
+console.log(increment(999));
+// .js "999" + 1 を"999" + "1"と解釈
---
+++ easy_func/increment.ts
@@ -0,0 +1,6 @@
+function increment(num: number) {
+    return num + 1;
+  }
+  
+  console.log(increment(999));
+  // .js "999" + 1 を"999" + "1"と解釈
